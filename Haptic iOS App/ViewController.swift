65ad4379e52f26ce4491969ca314bd71e53e4cb9
Alan Kantz <alan.kantz@gmail.com>
2018-03-10 17:05:57 -0500

    Initial Commit

---
+++ Haptic iOS App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Haptic iOS App
+//
+//  Created by Alan Kantz on 3/10/18.
+//  Copyright © 2018 Alan Kantz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
