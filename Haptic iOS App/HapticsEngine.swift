5658b0ddf257e053acf53c129bc1e5d2b1f13128
astruckm <astruckm@yahoo.com>
2018-03-10 19:13:33 -0500

    Haptics Engine started.

--- Haptic iOS App/HapticsEngine.swift
+++ Haptic iOS App/HapticsEngine.swift
@@ -8,4 +8,35 @@
 
 import UIKit
 
+class HapticsEngine {
+    //Dummy values for proof of concept
+    let beatDuration = 500
+    let songDuration = ((4 * 60) + 5) * 1000 //4'05 long song
+
+    var lightFeedbackGenerator: UIImpactFeedbackGenerator?
+    
+    //Will need to take in relevant audio metadata (e.g. loudness peaks)
+    func feedbackHandler() {
+        lightFeedbackGenerator = UIImpactFeedbackGenerator(style: .light)
+        lightFeedbackGenerator?.prepare()
+        
+        let timeOfSongEnd = Date(timeIntervalSinceNow: TimeInterval(songDuration / 1000))
+        Timer.scheduledTimer(withTimeInterval: TimeInterval(beatDuration / 1000), repeats: true) { timer in
+            if Date() > timeOfSongEnd {
+                timer.invalidate()
+            }
+            self.lightFeedbackGenerator?.impactOccurred()
+            print("beat")
+        }
+        lightFeedbackGenerator = nil
+    }
+    
+    func cancelFeedback() {
+        lightFeedbackGenerator = nil
+    }
+    
+    
+    
+    
+}
 
--- Haptic iOS App/ViewController.swift
+++ Haptic iOS App/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    var hapticEngine = HapticsEngine()
+    
+    @IBAction func testButton(_ sender: UIButton) {
+        hapticEngine.feedbackHandler()
+    }
+    func testTouch(_ sender: UIButton) {
+        hapticEngine.feedbackHandler()
+    }
+    
 }
 
